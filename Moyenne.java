d69cf7e64ec96d6d83c9f9f76fe64ce2d0126d50
Alexandra <alexafabree@gmail.com>
2017-03-28 10:44:22 +0200

    Add files via upload

---
+++ Moyenne.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Moyenne {
+	public static void main (String [] args) {
+		double valeur_stat;
+		int nombre_valeur;
+		double somme = 0;
+		Scanner sc = new Scanner (System.in);
+		System.out.println ("Veuillez saisir le nombre de valeurs");
+		nombre_valeur = sc.nextInt(); 
+		
+		for(int i=1; i <= nombre_valeur; i++) {
+			System.out.println ("Veuillez entrer vos valeurs");
+			valeur_stat = sc.nextFloat();
+			somme = somme + valeur_stat;
+			}
+		double valeur_moyenne = somme/nombre_valeur;
+		System.out.println("La moyenne de vos valeurs est :"+valeur_moyenne);
+	    
+	}
+}
