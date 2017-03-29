358df9b40da8b9727ca6d251e3a18659f4ddae59
Alexandra <alexafabree@gmail.com>
2017-03-29 10:59:11 +0200

    Update Exponentielle.java

--- Exponentielle.java
+++ Exponentielle.java
@@ -6,8 +6,8 @@ public class Exponentielle {
 	public static void main(String[]args){
 		float Exponentielle;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("veuillez donnée un chiffre ou un nombre: ");
+		System.out.println("Saisissez un chiffre ou un nombre: ");
 		Exponentielle = sc.nextFloat();
-		System.out.println("sa valeur exponentielle est de: " +Math.exp(Exponentielle));
+		System.out.println("Sa valeur exponentielle est de: " +Math.exp(Exponentielle));
 	}
 }
