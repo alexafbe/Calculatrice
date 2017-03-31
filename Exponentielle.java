e4b188dbae38e64c76f4a0369992bf3b7d6d0756
Paul <paulemile.gorre@ynov.com>
2017-04-01 00:45:41 +0200

    Update Exponentielle.java

--- Exponentielle.java
+++ Exponentielle.java
@@ -21,21 +21,30 @@ public class Exponentielle {
 		} while (isnumber != true);
 		System.out.println("Sa valeur exponentielle est de: " +Math.exp(Exponentielle1));
 		
-		int val;
+		int val = 0;
+		boolean isnumber1;
+		do { isnumber1 = true;
 		Scanner sb = new Scanner(System.in);
 		System.out.println("Voulez-vous ?");
 		System.out.println("1 : Recommencer ?");
 		System.out.println("2 : Retourner au menu ? (Ne fonctionne pas) ");
 		System.out.println("3 : Quitter?");
-		val = sb.nextInt();
-		while(val > 3 || val < 0){
-			System.out.println("Cette option n'existe pas!");
-			System.out.println("Voulez-vous ?");
-			System.out.println("1 : Recommencer ?");
-			System.out.println("2 : Retourner au menu ? (Ne fonctionne pas) ");
-			System.out.println("3 : Quitter?");
+		try{
 			val = sb.nextInt();
+			while(val > 3 || val < 0){
+				System.out.println("Cette option n'existe pas!");
+				System.out.println("Voulez-vous ?");
+				System.out.println("1 : Recommencer ?");
+				System.out.println("2 : Retourner au menu ? (Ne fonctionne pas) ");
+				System.out.println("3 : Quitter?");
+				val = sb.nextInt();
 		}
+		}catch (InputMismatchException e)
+		{
+			System.out.println("Erreur, la valeur saisie n'est pas un nombre");
+			isnumber1 = false;
+		}
+		}while (isnumber1 != true);
 		if (val == 1){
 			Exponentielle exponentielle = new Exponentielle();
 			exponentielle.Exponentielle();
