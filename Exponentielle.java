b4c95cca25179ed2e5e1c4bfdf6405446611e441
Alexandra <alexafabree@gmail.com>
2017-03-31 22:58:30 +0200

    Update Exponentielle.java

--- Exponentielle.java
+++ Exponentielle.java
@@ -4,10 +4,20 @@ import java.util.Scanner;
 
 public class Exponentielle {
 	public static void Exponentielle(){
-		float Exponentielle1;
+		float Exponentielle1 = 0;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Saisissez un chiffre ou un nombre: ");
-		Exponentielle1 = sc.nextFloat();
+		boolean isnumber;
+		do { isnumber = true;
+		System.out.println("Veuillez saisir un nombre afin de calculer sa valeur exponentielle :");
+		Scanner fcb = new Scanner (System.in);
+		try {
+		Exponentielle1 = fcb.nextInt();
+		} catch (InputMismatchException e)
+		{
+			System.out.println("Erreur, la valeur saisie n'est pas un nombre");
+			isnumber = false;
+		}
+		} while (isnumber != true);
 		System.out.println("Sa valeur exponentielle est de: " +Math.exp(Exponentielle1));
 		
 		int val;
