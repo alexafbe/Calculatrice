fec2a685449ce05df8dc8cb759a301fbb16bb186
Alexandra <alexafabree@gmail.com>
2017-03-31 23:47:39 +0200

    Update Puissance.java

--- Puissance.java
+++ Puissance.java
@@ -1,22 +1,51 @@
 package Calculatrice;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Puissance {
 	public static void Puissance(){
-		float cube;
-		float puissance1;
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Veuillez entrer un chiffre ou un nombre: ");
-		cube = sc.nextFloat();
+		float cube = 0;
+		float puissance1 = 0;
+		boolean isnumber;
+		do { isnumber = true;
+		System.out.println("Veuillez saisir un nombre :");
+		Scanner fcb = new Scanner (System.in);
+		try {
+		cube = fcb.nextFloat();
+		} catch (InputMismatchException e)
+		{
+			System.out.println("Erreur, la valeur saisie n'est pas un nombre");
+			isnumber = false;
+		}
+		} while (isnumber != true);
+		do { isnumber = true;
 		System.out.println("Veuillez indiquer sa puissance d'augmentation: ");
-		puissance1 = sc.nextFloat();
+		Scanner fcb = new Scanner (System.in);
+		try {
+		puissance1 = fcb.nextFloat();
+		} catch (InputMismatchException e)
+		{
+			System.out.println("Erreur, la valeur saisie n'est pas un nombre");
+			isnumber = false;
+		}
+		} while (isnumber != true);
 		while(puissance1 < 0){
 			System.out.println("Une puissance ne peut pas etre negative!");
+			do { isnumber = true;
 			System.out.println("Veuillez indiquer sa puissance d'augmentation: ");
-			puissance1 = sc.nextFloat();
+			Scanner fcb = new Scanner (System.in);
+			try {
+			puissance1 = fcb.nextFloat();
+			} catch (InputMismatchException e)
+			{
+				System.out.println("Erreur, la valeur saisie n'est pas un nombre");
+				isnumber = false;
+			}
+			} while (isnumber != true);
 		}
 		System.out.println("Sa valeur est de: " +Math.pow(cube,puissance1));
+		
 		int val;
 		Scanner sb = new Scanner(System.in);
 		System.out.println("Voulez-vous ?");
